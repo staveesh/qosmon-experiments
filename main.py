893e2f0289178b5c6cd8493a0cfdb46755c17e66
staveesh <sharmataveesh@yahoo.in>
2022-02-20 07:40:39 +0200

    Updated code

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import util
 
 def generate_topology(num_hosts):
     result = {}
-    num_switches = num_hosts
+    num_switches = num_hosts + 1
     num_targets = 1
     result['nSwitches'] = num_switches
     result['nHosts'] = num_hosts
@@ -79,22 +79,61 @@ containernet = {
     'password': 'containernet'
 }
 algorithms = ['random', 'rr', 'edf', 'dosd', 'aosd']
+n_jobs = 50
+
+topos = range(10, 16)
+
+exp_config = [(x, y) for x in topos for y in algorithms]
+
+current_directory = '/home/taveesh/Desktop/qosmon-experiments'
 
 
 def go():
-    experiment_id = 'experiment_{}_{}'.format(6, 10)
-    if not os.path.isdir(os.path.join(data_path, experiment_id)):
-        util.create_dir(os.path.join(data_path, experiment_id))
-    docker_client = docker.from_env()
-    n_hosts = 6
-    n_targets = 1
-    idx = 0
-    repeat = False
-    from_algo = 'rr'
-    for algorithm in algorithms:
+    exp_idx = 0
+    os.system('docker stop app influxdb mongodb')
+    os.system('docker rm app influxdb mongodb')
+    os.system('docker network rm backend')
+    print('Stopping remote Ryu controller...')
+    r = requests.get(controller_address + '/end')
+    print(r)
+    topo_id = topos[0]
+    for exp in os.listdir(data_path):
+        topo_id = max(topo_id, int(exp.split('_')[1]))
+    print('topo_id = ', topo_id)
+    new_exp = os.path.join(data_path, 'experiment_{}'.format(str(topo_id)))
+    if os.path.exists(new_exp):
+        os.chdir(new_exp)
+        result_files = [exp_file for exp_file in os.listdir(os.getcwd()) if exp_file.startswith('influx')]
+        offset = -1
+        for algo in algorithms:
+            file_name = 'influx_results_' + algo + '.csv'
+            if file_name not in result_files:
+                break
+            offset += 1
+
+        if offset == -1:
+            topo_id += 1
+            offset += 1
+
+        exp_idx = (topo_id - topos[0]) * len(algorithms) + offset + 1
+    else:
+        exp_idx = 0
+    os.chdir(current_directory)
+
+    print('Resuming experiments from index : ', exp_idx)
+
+    for experiment in exp_config[exp_idx:]:
+        topo = experiment[0]
+        algorithm = experiment[1]
+        experiment_id = 'experiment_{}'.format(topo)
+        if not os.path.isdir(os.path.join(data_path, experiment_id)):
+            util.create_dir(os.path.join(data_path, experiment_id))
+        docker_client = docker.from_env()
+        n_targets = 1
+        idx = algorithms.index(algorithm)
         # Server side
         print('#' * 40)
-        print('Configuration : n_devices = {} jtd = {} algo = {}'.format(6, 10, algorithm.upper()))
+        print('Configuration : n_devices = {} algo = {}'.format(topo, algorithm.upper()))
         env_vars['SCHEDULING_ALGO_NAME'] = algorithm
         backend = docker_client.networks.create('backend', driver='bridge')
         influx_container = docker_client.containers.run("influxdb:1.8", detach=True, name='influxdb',
@@ -126,7 +165,7 @@ def go():
                                                            '/home/taveesh/Desktop/qosmon/mongodb': {
                                                                'bind': '/data/db',
                                                                'mode': 'rw'}})
-        sleep(20)
+        sleep(120)
         app_container = docker_client.containers.run("staveesh/qosmon:latest", detach=True, name='app',
                                                      environment=env_vars, network='backend',
                                                      ports={'7800/tcp': 7800},
@@ -140,45 +179,42 @@ def go():
         r = requests.get(controller_address + '/start')
         print(r)
         # Client side
-        print('Launching VirtualBox....')
-        os.system('VBoxManage startvm "SDN" --type separate')
-        sleep(60)
-        print('Attempting to connect to VirtualBox instance...')
-
-        session = paramiko.SSHClient()
-        session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        session.connect(hostname=containernet['host'],
-                        username=containernet['username'],
-                        password=containernet['password'])
-        print('Connected')
-        print('Copying topology file...')
-        sftp = session.open_sftp()
-        sftp.put(os.path.join(os.getcwd(), 'topos/topo-{}.json'.format(6)),
-                 '/home/' + containernet['username'] + '/topo-{}.json'.format(6))
-        sleep(10)
-        sftp.close()
-        print('Initiating python script....')
-        stdin, stdout, stderr = session.exec_command(
-            "sudo -S -p '' python topology.py {}".format('topo-{}.json'.format(6)))
-        stdin.write(containernet['password'] + '\n')
-        stdin.flush()
-        sleep(30)
+        # print('Launching VirtualBox....')
+        # os.system('VBoxManage startvm "SDN" --type separate')
+        sleep(120)
+        # print('Attempting to connect to VirtualBox instance...')
+        #
+        # session = paramiko.SSHClient()
+        # session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # session.connect(hostname=containernet['host'],
+        #                 username=containernet['username'],
+        #                 password=containernet['password'])
+        # print('Connected')
+        # print('Copying topology file...')
+        # sftp = session.open_sftp()
+        # sftp.put(os.path.join(os.getcwd(), 'topos/topo-{}.json'.format(topo)),
+        #          '/home/' + containernet['username'] + '/topo-{}.json'.format(topo))
+        # sleep(120)
+        # sftp.close()
+        # print('Cleaning mininet')
+        # stdin, stdout, stderr = session.exec_command("sudo -S -p '' mn -c")
+        # stdin.write(containernet['password'] + '\n')
+        # stdin.flush()
+        # sleep(120)
+        # print('Initiating python script....')
+        # stdin, stdout, stderr = session.exec_command(
+        #     "sudo -S -p '' python topology.py {}".format('topo-{}.json'.format(topo)))
+        # stdin.write(containernet['password'] + '\n')
+        # stdin.flush()
+        # sleep(120)
         print("Scheduling jobs...")
-        if idx == 0:
-            if repeat:
-                scheduler.repeat_with_data(os.path.join(data_path, experiment_id, 'exp_' + from_algo),
-                                           os.path.join(data_path, experiment_id), 'exp_' + algorithm)
-            else:
-                scheduler.schedule('exp_' + algorithm, os.path.join(data_path, experiment_id), n_targets,
-                                   60)
-        else:
-            scheduler.repeat_with_data(os.path.join(data_path, experiment_id, 'exp_' + algorithms[idx - 1]),
-                                       os.path.join(data_path, experiment_id), 'exp_' + algorithm)
-        sleep(4200)  # 1 hr 10 minutes
+        scheduler.repeat_with_data(os.path.join(current_directory, 'jobs', 'exp_job'),
+                                   os.path.join(data_path, experiment_id), 'exp_' + algorithm)
+        sleep(11400)  # 3 hr 10 minutes
         # Cleanup
         print('Collecting data...')
         collector.collect(os.path.join(data_path, experiment_id), 'exp_' + algorithm)
-        sleep(10)
+        sleep(120)
         influx_client = InfluxDBClient(host='localhost', port=8086)
         influx_client.switch_database('mobiperf')
         measurements = [dct['name'] for dct in influx_client.get_list_measurements()]
@@ -189,7 +225,7 @@ def go():
                 points.append(point)
             df = pd.DataFrame(points)
             df.to_csv(os.path.join(data_path, experiment_id, 'influx_results_' + algorithm + '.csv'))
-            sleep(10)
+            sleep(120)
         print('Cleaning up influxdb...')
         influx_client.query('DROP SERIES FROM /.*/')
         influx_client.close()
@@ -218,11 +254,11 @@ def go():
         err = stderr.read().decode()
         if err:
             print(err)
-        sleep(10)
+        sleep(120)
         command = "sudo docker stop "
         command += " ".join(["mn.t" + str(i) for i in range(1, n_targets + 1)])
         command += " "
-        command += " ".join(["mn.h" + str(i) for i in range(1, n_hosts + 1)])
+        command += " ".join(["mn.h" + str(i) for i in range(1, topo + 1)])
         print(command)
         stdin, stdout, stderr = session.exec_command(command, get_pty=True)
         stdin.write(containernet['password'] + '\n')
@@ -231,11 +267,11 @@ def go():
         err = stderr.read().decode()
         if err:
             print(err)
-        sleep(10)
+        sleep(120)
         command = "sudo docker rm "
         command += " ".join(["mn.t" + str(i) for i in range(1, n_targets + 1)])
         command += " "
-        command += " ".join(["mn.h" + str(i) for i in range(1, n_hosts + 1)])
+        command += " ".join(["mn.h" + str(i) for i in range(1, topo + 1)])
         print(command)
         stdin, stdout, stderr = session.exec_command(command, get_pty=True)
         stdin.write(containernet['password'] + '\n')
@@ -266,4 +302,5 @@ def go():
 
 
 if __name__ == '__main__':
+    # scheduler.schedule('exp_job', os.path.join(data_path, 'jobs'), 1, n_jobs)
     go()
--- scheduler.py
+++ scheduler.py
@@ -8,14 +8,14 @@ import util
 
 JOB_TYPES = ["tcp_speed_test"]
 
-START_TIME_INTERVAL = (1, 5)
-JOB_PERIOD_INTERVAL = (5, 10)
+START_TIME_INTERVAL = (1, 10)
+JOB_PERIOD_INTERVAL = (10, 20)
 server_endpoint = "http://localhost:7800/schedule"
 
 
 def schedule(exp, destination, n_targets, n_jobs):
     EXP_START_TIME = datetime.datetime.utcnow()
-    END_TIME = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+    END_TIME = datetime.datetime.utcnow() + datetime.timedelta(hours=3)
     TARGET_SERVERS = ["10.0.1."+str(i) for i in range(1, n_targets+1)]
     for i in range(1, n_jobs + 1):
         minutes_ahead = random.randint(START_TIME_INTERVAL[0], START_TIME_INTERVAL[1])
@@ -49,7 +49,7 @@ def schedule(exp, destination, n_targets, n_jobs):
             "requestType": "SCHEDULE_MEASUREMENT",
             "userId": "taveesh08@gmail.com",
         }
-        r = requests.post(server_endpoint, json=schedule_request)
+        # r = requests.post(server_endpoint, json=schedule_request)
         write_request_to_file(job_key, schedule_request, destination, exp)
 
 
@@ -62,14 +62,14 @@ def write_request_to_file(job_key, schedule_request, output_dir, exp):
 
 def repeat_with_data(from_dir, output_dir, exp):
     EXP_START_TIME = datetime.datetime.utcnow()
-    END_TIME = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+    END_TIME = datetime.datetime.utcnow() + datetime.timedelta(hours=3)
     for data_file in os.listdir(from_dir):
         with open(os.path.join(from_dir, data_file), "r") as json_file:
             schedule_request = json.load(json_file)
             md = schedule_request['jobDescription']['measurementDescription']
             previous_start = dateutil.parser.parse(md['startTime'])
             previous_end = dateutil.parser.parse(md['endTime'])
-            minutes_ahead = (previous_start - (previous_end - datetime.timedelta(hours=1)))
+            minutes_ahead = (previous_start - (previous_end - datetime.timedelta(hours=3)))
             start_time = EXP_START_TIME + minutes_ahead
             md['startTime'] = start_time.replace(microsecond=0).isoformat() + ".000Z"
             md['endTime'] = END_TIME.replace(microsecond=0).isoformat() + ".000Z"
